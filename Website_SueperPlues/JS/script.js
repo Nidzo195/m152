41b22c911a66f7dcacf1ec837d7151910197a861
Nidzo195 <65396788+Nidzo195@users.noreply.github.com>
2020-06-26 11:30:13 +0200

    Add files via upload

---
+++ Website_SueperPlues/JS/script.js
@@ -0,0 +1,296 @@
+function turnCar() {
+
+   //   document.getElementById('start').style.backgroundImage = "url('Pictures/bmw.jpg')";
+      document.getElementsByTagName("a")[4].setAttribute("href", "HTML/product.html#");
+      document.getElementById('interduction').style.paddingTop = "50px";
+
+}
+function sendForm(){
+  alert("Your request was sent!\nWe will contact you as soon as we can!");
+  alert(total);
+}
+
+async function HistoryToGoals(){
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.9";
+  document.getElementById("arrowLeft").style.opacity = "0.9";
+  document.getElementById("circles").style.opacity = "0.9";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.8";
+  document.getElementById("arrowLeft").style.opacity = "0.8";
+  document.getElementById("circles").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.7";
+  document.getElementById("arrowLeft").style.opacity = "0.7";
+  document.getElementById("circles").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.6";
+  document.getElementById("arrowLeft").style.opacity = "0.6";
+  document.getElementById("circles").style.opacity = "0.6";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.5";
+  document.getElementById("arrowLeft").style.opacity = "0.5";
+  document.getElementById("circles").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.4";
+  document.getElementById("arrowLeft").style.opacity = "0.4";
+  document.getElementById("circles").style.opacity = "0.4";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.3";
+  document.getElementById("arrowLeft").style.opacity = "0.3";
+  document.getElementById("circles").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.2";
+  document.getElementById("arrowLeft").style.opacity = "0.2";
+  document.getElementById("circles").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 70));
+  document.getElementById("slideshow").style.opacity = "0.9";
+  document.getElementById("history").style.opacity = "0.1";
+  document.getElementById("arrowLeft").style.opacity = "0.1";
+  document.getElementById("circles").style.opacity = "0.1";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.6";
+  document.getElementById("history").style.opacity = "0.0";
+  document.getElementById("arrowLeft").style.opacity = "0.0";
+  document.getElementById("circles").style.opacity = "0.0";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.4";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.1";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.0";
+
+//Main Part
+  document.getElementById('arrowLeft').style.visibility = "hidden" ;
+  document.getElementById('arrowRight').style.visibility = "visible";
+  document.getElementById("circleLeft").classList.add('far');
+  document.getElementById("circleLeft").classList.remove('fas');
+  document.getElementById("circleRight").classList.add('fas');
+  document.getElementById("circleRight").classList.remove('far');
+  var image = document.getElementById("PictureSlide");
+  image.src = "../Pictures/goals.jpg";
+  document.getElementById('TextSlide').innerHTML = "2. (Goals): Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+  document.getElementById('TitelSlide').innerHTML = "Goals";
+
+  document.getElementById("slideshow").style.opacity = "0.1";
+  document.getElementById("history").style.opacity = "0.1";
+  document.getElementById("arrowRight").style.opacity = "0.1";
+  document.getElementById("circles").style.opacity = "0.1";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.4";
+  document.getElementById("history").style.opacity = "0.2";
+  document.getElementById("arrowRight").style.opacity = "0.2";
+  document.getElementById("circles").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.6";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.9";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "1.0";
+  await new Promise(r => setTimeout(r, 70));
+  document.getElementById("history").style.opacity = "0.3";
+  document.getElementById("arrowRight").style.opacity = "0.3";
+  document.getElementById("circles").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.4";
+  document.getElementById("arrowRight").style.opacity = "0.4";
+  document.getElementById("circles").style.opacity = "0.4";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.5";
+  document.getElementById("arrowRight").style.opacity = "0.5";
+  document.getElementById("circles").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.6";
+  document.getElementById("arrowRight").style.opacity = "0.6";
+  document.getElementById("circles").style.opacity = "0.6";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.7";
+  document.getElementById("arrowRight").style.opacity = "0.7";
+  document.getElementById("circles").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.8";
+  document.getElementById("arrowRight").style.opacity = "0.8";
+  document.getElementById("circles").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.9";
+  document.getElementById("arrowRight").style.opacity = "0.9";
+  document.getElementById("circles").style.opacity = "0.9";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "1.0";
+  document.getElementById("arrowRight").style.opacity = "1.0";
+  document.getElementById("circles").style.opacity = "1.0";
+}
+
+async function GoalsToHistory(){
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.9";
+  document.getElementById("arrowRight").style.opacity = "0.9";
+  document.getElementById("circles").style.opacity = "0.9";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.8";
+  document.getElementById("arrowRight").style.opacity = "0.8";
+  document.getElementById("circles").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.7";
+  document.getElementById("arrowRight").style.opacity = "0.7";
+  document.getElementById("circles").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.6";
+  document.getElementById("arrowRight").style.opacity = "0.6";
+  document.getElementById("circles").style.opacity = "0.6";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.5";
+  document.getElementById("arrowRight").style.opacity = "0.5";
+  document.getElementById("circles").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.4";
+  document.getElementById("arrowRight").style.opacity = "0.4";
+  document.getElementById("circles").style.opacity = "0.4";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.3";
+  document.getElementById("arrowRight").style.opacity = "0.3";
+  document.getElementById("circles").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.2";
+  document.getElementById("arrowRight").style.opacity = "0.2";
+  document.getElementById("circles").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 70));
+  document.getElementById("slideshow").style.opacity = "0.9";
+  document.getElementById("history").style.opacity = "0.1";
+  document.getElementById("arrowRight").style.opacity = "0.1";
+  document.getElementById("circles").style.opacity = "0.1";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.6";
+  document.getElementById("history").style.opacity = "0.0";
+  document.getElementById("arrowRight").style.opacity = "0.0";
+  document.getElementById("circles").style.opacity = "0.0";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.4";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.1";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.0";
+
+//Main Part
+  document.getElementById('arrowLeft').style.visibility = "visible";
+  document.getElementById('arrowRight').style.visibility = "hidden";
+  document.getElementById("circleLeft").classList.add('fas');
+  document.getElementById("circleLeft").classList.remove('far');
+  document.getElementById("circleRight").classList.add('far');
+  document.getElementById("circleRight").classList.remove('fas');
+  var image = document.getElementById("PictureSlide");
+  image.src = "../Pictures/history.jpg";
+  document.getElementById('TextSlide').innerHTML = "1. (History): Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+  document.getElementById('TitelSlide').innerHTML = "History";
+
+  document.getElementById("slideshow").style.opacity = "0.1";
+  document.getElementById("history").style.opacity = "0.1";
+  document.getElementById("arrowLeft").style.opacity = "0.1";
+  document.getElementById("circles").style.opacity = "0.1";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.4";
+  document.getElementById("history").style.opacity = "0.2";
+  document.getElementById("arrowLeft").style.opacity = "0.2";
+  document.getElementById("circles").style.opacity = "0.2";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.6";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "0.9";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("slideshow").style.opacity = "1.0";
+  await new Promise(r => setTimeout(r, 70));
+  document.getElementById("history").style.opacity = "0.3";
+  document.getElementById("arrowLeft").style.opacity = "0.3";
+  document.getElementById("circles").style.opacity = "0.3";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.4";
+  document.getElementById("arrowLeft").style.opacity = "0.4";
+  document.getElementById("circles").style.opacity = "0.4";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.5";
+  document.getElementById("arrowLeft").style.opacity = "0.5";
+  document.getElementById("circles").style.opacity = "0.5";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.6";
+  document.getElementById("arrowLeft").style.opacity = "0.6";
+  document.getElementById("circles").style.opacity = "0.6";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.7";
+  document.getElementById("arrowLeft").style.opacity = "0.7";
+  document.getElementById("circles").style.opacity = "0.7";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.8";
+  document.getElementById("arrowLeft").style.opacity = "0.8";
+  document.getElementById("circles").style.opacity = "0.8";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "0.9";
+  document.getElementById("arrowLeft").style.opacity = "0.9";
+  document.getElementById("circles").style.opacity = "0.9";
+  await new Promise(r => setTimeout(r, 50));
+  document.getElementById("history").style.opacity = "1.0";
+  document.getElementById("arrowLeft").style.opacity = "1.0";
+  document.getElementById("circles").style.opacity = "1.0";
+}
+
+
+$("#tankstellen").hover(function () {
+
+  var isTankstelleHover = $('#tankstellen').is( ":hover" );
+
+  if(isTankstelleHover){
+      $('#tankstellen').css("background-color", "#ff007f");//pink
+  }else{
+      $('#tankstellen').css("background-color", "#2E2EFE");//blau
+  }
+
+});
+
+$("#countries").hover(function () {
+
+      var isCountriesHover = $('#countries').is( ":hover" );
+
+      if(isCountriesHover){
+      $('#tankstellen').css("background-color", "#ff0000");//rot
+      }
+
+});
